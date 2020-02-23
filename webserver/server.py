ba90b8a9a84387e0a90d5605d77c6e73c667cfd4
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-02-23 06:42:50 -0500

    webpages are dynamically rendered

--- webserver/server.py
+++ webserver/server.py
@@ -1,31 +1,14 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 print(__name__)
 
+
 @app.route('/')
-def defaultpage():
+def home_page():
     return render_template('index.html')
 
-@app.route('/index.html')
-def homepage():
-    return render_template('index.html')
 
-@app.route('/work.html')
-def workpage():
-    return render_template('work.html')
-
-@app.route('/works.html')
-def workspage():
-    return render_template('works.html')
-
-@app.route('/contact.html')
-def contactpage():
-    return render_template('contact.html')
-
-@app.route('/components.html')
-def componentspage():
-    return render_template('components.html')
-
-@app.route('/about.html')
-def aboutpage():
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def dynamic_page(page_name):
+    return render_template(page_name)
