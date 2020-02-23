8ea1663a58a8016dff14244c27bd433de25235d8
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-02-23 06:14:27 -0500

    playing around with flask

--- webserver/server.py
+++ webserver/server.py
@@ -3,5 +3,29 @@ app = Flask(__name__)
 print(__name__)
 
 @app.route('/')
-def hell_world():
+def defaultpage():
     return render_template('index.html')
+
+@app.route('/index.html')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/work.html')
+def workpage():
+    return render_template('work.html')
+
+@app.route('/works.html')
+def workspage():
+    return render_template('works.html')
+
+@app.route('/contact.html')
+def contactpage():
+    return render_template('contact.html')
+
+@app.route('/components.html')
+def componentspage():
+    return render_template('components.html')
+
+@app.route('/about.html')
+def aboutpage():
+    return render_template('about.html')
